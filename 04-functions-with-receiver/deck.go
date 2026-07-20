42ccedb09e7e41ceecf45465eca6a8f113a4a479
mjhddev <mujahid.170997@gmail.com>
2026-07-20 15:05:20 +0700

    docs: add comments to deck functions

--- 04-functions-with-receiver/deck.go
+++ 04-functions-with-receiver/deck.go
@@ -8,6 +8,7 @@ import (
 
 type deck []string
 
+// newDeck creates and returns a new deck of cards
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
@@ -22,24 +23,29 @@ func newDeck() deck {
 	return cards
 }
 
+// print prints out all the cards in the deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
+// deal splits a deck into two decks, one with the specified hand size and the other with the remaining cards
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
+// toString converts the deck to a single string with cards separated by commas
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
+// saveToFile saves the deck to a file with the specified filename
 func (d deck) saveToFile(filename string) error {
 	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
+// newDeckFromFile reads a deck from a file with the specified filename and returns it as a deck
 func newDeckFromFile(filename string) deck {
 	bs, err := os.ReadFile(filename)
 	if err != nil {
