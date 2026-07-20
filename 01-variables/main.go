43a97be532ad4ba6ce55346ea26280ee7da7f113
mjhddev <mujahid.170997@gmail.com>
2026-07-20 10:10:06 +0700

    feat: add variables lesson

--- 01-variables/main.go
+++ 01-variables/main.go
@@ -3,7 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	fmt.Println(card)
+	// Menggunakan keyword var
+	var name string = "Mujahid"
+
+	// Type inference
+	var age = 25
+
+	// Short variable declaration
+	city := "Bandar Lampung"
+
+	// Multiple variable declaration
+	job, active := "Backend Developer", true
+
+	fmt.Println("Name  :", name)
+	fmt.Println("Age   :", age)
+	fmt.Println("City  :", city)
+	fmt.Println("Job   :", job)
+	fmt.Println("Active:", active)
 }
